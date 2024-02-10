ac1db9e41e4310e00588b31f783104f8ca050dbf
PeterSteiglechner <peter.steiglechner@gmail.com>
2024-02-10 14:24:16 +0100

    formatting

--- Extensions/model_uniformNoise_forFigureS19.py
+++ Extensions/model_uniformNoise_forFigureS19.py
@@ -1,5 +1,5 @@
-# Opinion formation model with BC and different types of noise
-# November 2023
+# Opinion formation model with bounded confidence and different types of noise
+# final edit December 2023
 # Update: Here, the noise is drawn form a bounded uniform distribution!
 # Peter Steiglechner
 # peter.steiglechner@gmail.com
@@ -20,7 +20,7 @@ warnings.filterwarnings('ignore')
 class Model:
     def __init__(self, params, track_times, noise_type, **kwargs):
         """
-        Create model by initialising an array of agents with opinions 
+        Create model by initialising an array of agent opinions 
         and defining how these are updated in an update step.  
         """
         self.params = params
@@ -29,7 +29,7 @@ class Model:
 
         # Define Model parameters
         self.n_agents = params["n_agents"]          # Number of agents
-        self.mu = params["mu"]                # convergence rate
+        self.mu = params["mu"]                      # convergence rate
         self.nu = params["nu"]                      # noise level 
         self.bc = params["bc"]                      # bounded confidence radius (bias)
         self.initial_dist = params["initial_dist"]  
@@ -50,18 +50,19 @@ class Model:
         self.all_x[0,:] = self.x.astype("f4") 
         return 
 
+
     def run(self, t_simulation):
         """
         update all agents for t_simulation times, store values if time is in track_times
         """
-        _abs = abs  # for performance
+        _abs = abs  # for simulation speed
 
         lower = 0.0  # edge of opinion space
         upper = 1.0  # edge of opinion space
         
         interaction_schedule = np.random.randint(0, self.n_agents, size=(t_simulation + 2, 2))
         
-        # remove monologues (agents talking to themselves)
+        # remove agent monologues (agents communicating with themselves)
         monologues = np.where(interaction_schedule[:,0]==interaction_schedule[:,1])
         if len(monologues)>0:
             interaction_schedule[monologues, 1] = (interaction_schedule[monologues, 1] + np.random.randint(0,self.n_agents, size=(len(monologues))))%self.n_agents
@@ -150,42 +151,23 @@ class Model:
             if counter >= 0.95*len(noises):
                     noises = np.random.normal(loc=0, scale=self.nu, size=int(t_simulation))
                     counter=0
-
-            # 5. Store opinions if time step is in track_times.
+                                
+            # 4. Store opinions if time step is in track_times.
             if t in self.track_times:
                 ind = self.track_times.index(t)
                 self.all_x[ind, :] = self.x.astype("f4")
+
         return 
 
 
 
 # INITIAL CONDITIONS
 
-
-# def init6G(n, sig, sixAm):
-#     """ 
-#     create a distribution of initial opinions from a superposition of 6 gaussian functions a0*N(1/12,sig)+a1*N(3/12,sig)+...
-#     where sixAm = (a0, ...a6)
-#     """
-#     gauss = lambda x, mu, sig: 1/(sig*np.sqrt(2*np.pi)) * np.exp(-0.5 * (x-mu)**2/(sig)**2) 
-#     multigauss = lambda x, amp, mus, sig: np.sum( [a*gauss(x, mu, sig) for a,mu in zip(amp, mus)], axis=0) 
-#     xgrid = np.linspace(0,1,1000)
-#     segments = np.linspace(0, 1, num=len(sixAm)+1)[:len(sixAm)]
-#     pdf = list(multigauss(xgrid, amp= sixAm, mus = segments+np.diff(segments)[0]/2, sig=sig))
-#     cdf = np.cumsum(pdf) 
-#     cdf /= cdf[-1]
-#     initX = []
-#     for i in range(n):
-#         rand = np.random.random()
-#         densityChoice = min(cdf[cdf>=rand])
-#         initX.append(xgrid[list(cdf).index(densityChoice)])
-#     initX = np.array(initX)
-#     return initX
-
-
 def init2G(n, a0=0.07432029, mu0=0.34018943, sig0=0.25329727, a1=0.10308521, mu1=0.77908659, sig1=0.13464983):
     """ 
     create a distribution of initial opinions from a superposition of 2 gaussian functions a0*N(mu0,sig0)+a1*N(mu1,sig1)
+    
+    the values are fitted to obtain a distribution that resembles the distribution of the six Americas (Maibach 2011). 
     """
     gauss = lambda x, mu, sig: 1/(sig*np.sqrt(2*np.pi)) * np.exp(-0.5 * (x-mu)**2/(sig)**2) 
     twogauss = lambda x, a0, mu0, sig0, a1, mu1, sig1: a0 * gauss(x, mu0, sig0) + a1 * gauss(x, mu1, sig1) 
@@ -205,24 +187,17 @@ def init_opinions(initial_dist, n_agents):
     # initialise agents at t=0
     if initial_dist== "uniform":
         initX = stats.uniform(0,1).rvs(size=n_agents)  
-    #elif initial_dist=="6AM":
-    #    sixAm = np.array([0.07,0.11,0.12,0.18,0.33,0.19])  # data from Leiserowitz 2021 and Maibach et al 2011
-    #    segments = np.linspace(0, 1, num=len(sixAm)+1)[:len(sixAm)]
-    #    initX = np.random.choice(segments, size=n_agents, replace=True, p=initial_dist) + 1/len(segments) * np.random.random(size=n_agents)
     elif "2G" in initial_dist:
         if initial_dist == "2G-6AM":
             a0, mu0, sig0, a1, mu1, sig1 = (0.07432029, 0.34018943, 0.25329727, 0.10308521, 0.77908659, 0.13464983)  # fitted parameters
         #elif initial_dist=="2G-Poles":
         #    a0, mu0, sig0, a1, mu1, sig1 = (0.5,0.2,0.1,0.5,0.8,0.1)
         initX = init2G(n_agents, a0, mu0, sig0, a1, mu1, sig1)
-    #elif "6G-6AM" in initial_dist:
-    #    assert initial_dist[-4:-2]=="0."
-    #    sixAm = np.array([0.07,0.11,0.12,0.18,0.33,0.19])  # data from Leiserowitz 2021 and Maibach et al 2011
-    #    initX = init6G(n_agents, float(initial_dist[-4:]), sixAm)
-    #elif initial_dist == "beta":
-    #    # ESS8 data in Germany: variable "wrclmch" (how much do you worry about climate change?) 
-    #    # --> fit is a beta distribution.
-    #    initX = stats.beta(4.026928199803696, 3.028342634840655, 0, 1).rvs(n_agents)
+    elif "example1G" in initial_dist:
+        loc = float(initial_dist[-3:])
+        scale=0.1
+        a, b = (0- loc) / scale, (1 - loc) / scale
+        initX = stats.truncnorm(loc=loc, scale=scale, a=a, b=b).rvs(size=n_agents)  
     else:
         print("error in initial dist")
         return 0
@@ -277,64 +252,50 @@ def main(noise_type, expname, n, ic, mu_arr, seeds, track_times, resolution, ver
             )
         d = d.astype("f4")
         d.attrs["initial_dist"] = params["initial_dist"]
-        # STORE
         if len(seeds)==1: 
-            fname = f"data/model-{noise_type}_"+expname+"{}Initial_eps{:.3f}_seeds{}.ncdf".format(ic,  eps, seeds[0])
+            fname = f"data/model-UNIFORM{noise_type}_"+expname+"{}Initial_eps{:.3f}_seeds{}.ncdf".format(ic,  eps, seeds[0])
         else:
-            fname = f"data/model-{noise_type}_"+expname+"{}Initial_eps{:.3f}_seeds{}-{}.ncdf".format(ic,  eps, seeds[0], seeds[-1])
+            fname = f"data/model-UNIFORM{noise_type}_"+expname+"{}Initial_eps{:.3f}_seeds{}-{}.ncdf".format(ic,  eps, seeds[0], seeds[-1])
         print("saved: ", fname)
         d.to_netcdf(fname)
     if verbose: print("finished")
 
     if len(seeds)==1:
-        ncdfs = [xr.open_dataset(f"data/model-{noise_type}_{expname}{ic}Initial_eps{eps:.3f}_seeds{s}.ncdf", engine="netcdf4") for eps in eps_arr]
+        ncdfs = [xr.open_dataset(f"data/model-UNIFORM{noise_type}_{expname}{ic}Initial_eps{eps:.3f}_seeds{s}.ncdf", engine="netcdf4") for eps in eps_arr]
         ncdfs_merged = xr.merge(ncdfs)
         ncdfs_merged.attrs["initial_dist"] = params["initial_dist"]
-        ncdfs_merged.to_netcdf(f"data/model-{noise_type}_{expname}{ic}Initial_seeds{s}.ncdf")
+        ncdfs_merged.to_netcdf(f"data/model-UNIFORM{noise_type}_{expname}{ic}Initial_seeds{s}.ncdf")
     return 
 
 
 epsarrs = {
     "high": np.insert(np.arange(0.025,0.41, step=0.025), 0, 1e-3),   # high resolution of the phase space
     "low": np.insert(np.arange(0.05,0.41, step=0.05), 0, 1e-3)  ,     # low resolution of the phase space
-    "highInv": np.append(np.insert(1/np.arange(3, 21)[::-1], 0,1/100), [1/2.5]),  # high resolution of the phase space
-    "lowInv": 1/np.array([2,3,4,5,6,7,8,9,10,12,15,20])     # low resolution of the phase space
+    "SA": [0.05, 0.175, 0.3 ]
 }
 nuarrs = {
     "high": np.concatenate([np.array([1e-10,1e-3, 2e-3, 5e-3]), np.arange(0.01,0.301, 0.01)]),
-    "low": np.concatenate([np.array([1e-10,1e-3]), np.arange(0.03,0.301, 0.03)])
+    "low": np.concatenate([np.array([1e-10,1e-3]), np.arange(0.03,0.301, 0.03)]),
+    "SA": [0.05,0.08, 0.13,0.20]
 }
- 
-        
+
+
 if __name__=="__main__":
 
-    import cProfile
-    import pstats
-    with cProfile.Profile() as pr:
-        s0 = time.time()
-        
-        # CHOOSE PARAMETERS 
-        track_times = np.arange(0, int(1e4)+1, step=10000) #  np.arange(0, int(1e5)+1, step=10000) 
-        mu_arr = [0.5]
-        n = 100
-        seeds = list(range(3))    # list(range(1000))
-        resolution = "low"
-        ic = "2G-6AM"
-        noise_type = "ambiguityNoise"
-        assert noise_type in ["ambiguityNoise", "selectionNoise", "exogenousNoise", "adaptationNoise"]
-
-        main(noise_type, resolution+"Res_" , n, ic, mu_arr, seeds, track_times, resolution=resolution, verbose=False)
-
-        #main(noise_type, expname, n, ic, mu_arr, seeds, track_times, resolution=resolution, verbose=True)
-        s1 = time.time()
-        print("{} min {} sec".format(int((s1-s0)/60), int(s1-s0)%60 ))
-    stats = pstats.Stats(pr)
-    stats.sort_stats(pstats.SortKey.TIME)
-    # Now you have two options, either print the data or save it as a file
-    #stats.print_stats() # Print The Stats
-    #stats.dump_stats("cprofile.prof")
+    s0 = time.time()
     
-    
+    # CHOOSE PARAMETERS 
+    track_times = np.arange(0, int(1e5)+1, step=1000)
+    mu_arr = [0.5]
+    n = 100
+    seeds = list(range(10))    # list(range(1000))
+    resolution = "SA"
+    ic = "2G-6AM"
+    noise_type = "ambiguityNoise"   # in ["ambiguityNoise", "selectionNoise", "exogenousNoise", "adaptationNoise"]
+    main(noise_type, resolution+"Res_" , n, ic, mu_arr, seeds, track_times, resolution=resolution, verbose=False)
+
+    s1 = time.time()
+    print("{} min {} sec".format(int((s1-s0)/60), int(s1-s0)%60 ))
 
 
 
